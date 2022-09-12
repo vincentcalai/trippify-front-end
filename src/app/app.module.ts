8c4073f14afab83af03be655b12894e9e6e607bb
Vincent Lai <vincentlai91@hotmail.com>
2022-09-12 13:41:13 +0800

    added home component, welcome banner + message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './views/a-main/app.component';
 import { HeaderComponent } from './views/b-header/header/header.component';
+import { HomeComponent } from './views/c-content/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
