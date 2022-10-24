b42fdc7813efaead95e38ef43e29bc0bf16d2495
Vincent Lai <vincentlai91@hotmail.com>
2022-10-24 11:13:57 +0800

    added test case for create trip details

--- src/app/create-trip/create-trip-details-form/create-trip-details-form.component.spec.ts
+++ src/app/create-trip/create-trip-details-form/create-trip-details-form.component.spec.ts
@@ -4,6 +4,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { BsModalService } from 'ngx-bootstrap/modal';
+import { Destinations } from 'src/app/model/destinations.model';
 import { ReactiveFormService } from 'src/app/services/reactive-form.service';
 import { SharedMethods } from 'src/app/services/shared-methods.service';
 import { SharedVar } from 'src/app/services/shared-var.service';
@@ -45,5 +46,29 @@ describe('CreateTripDetailsFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit test case, with stored value and clicked prev page', () => {
+    let destinations: Destinations[] = [];
+    let destination = new Destinations();
+    destination.cityName = "New York";
+    destination.ctryName = "United States";
+    destination.dateFrom = {year: 2022, month: 10, day: 24};
+    destination.dateTo = {year: 2022, month: 10, day: 31};
+    destination.dateFromDayName = 'MON';
+    destination.dateToDayName = 'MON';
+    destination.dateFromStr = '20221024';
+    destination.dateToStr = '20221031';
+    destination.noOfTripDays = 8;
+    destinations.push(destination);
+
+    component.sharedVar.createTripModel.tripDetails.noOfDestinations = 1;
+    component.sharedVar.createTripModel.tripDetails.destinations = destinations;
+
+    component.ngOnInit();
+
+    fixture.detectChanges();
+    expect(component.staticQn2.value).toBe(1);
+    expect(component.createTripDetailsForm.valid).toBe(true);
   });
 });
