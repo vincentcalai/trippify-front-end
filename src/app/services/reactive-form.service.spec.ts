3a87ec11358a718ca263841c29d86b9ebd51a71e
Vincent Lai <vincentlai91@hotmail.com>
2022-10-25 19:05:07 +0800

    updated test cases for reactive-form and shared-methods.service.spec.ts

--- src/app/services/reactive-form.service.spec.ts
+++ src/app/services/reactive-form.service.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from '../shared/shared.module';
 
@@ -27,5 +27,64 @@ describe('ReactiveFormService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('init destination form group valid - trip date from is not later than date to', () => {
+    let testFormGroup = service.initDestinationFormGrp();
+    testFormGroup.get('ctryName').setValue('SINGAPORE');
+    testFormGroup.get('cityName').setValue('SINGAPORE');
+    testFormGroup.get('dateFrom').setValue('25/10/2022');
+    testFormGroup.get('dateTo').setValue('31/10/2022');
+    expect(testFormGroup.valid).toBe(true);
+  });
+
+  it('init destination form group invalid - trip date from is later than date to', () => {
+    let testFormGroup = service.initDestinationFormGrp();
+    testFormGroup.get('ctryName').setValue('SINGAPORE');
+    testFormGroup.get('cityName').setValue('SINGAPORE');
+    testFormGroup.get('dateFrom').setValue('31/10/2022');
+    testFormGroup.get('dateTo').setValue('30/10/2022');
+    expect(testFormGroup.valid).toBe(false);
+  });
+
+  it('displayValidationErrors test cases', () => {
+    const mockFormControl = service.fb.control(null);
+    const mockFormGroup = service.fb.group({
+      mockControl1: service.fb.control(null),
+      mockControl2: service.fb.control(null),
+    });
+    const mockFormGroupWithFormArray = service.fb.group({
+      mockFormArray: service.fb.array([
+        service.fb.group({ mockControl: service.fb.control(null) }),
+        service.fb.group({ mockControl: service.fb.control(null) }),
+      ])
+    })
+    expect(mockFormControl.dirty).toBe(false);
+    expect(mockFormControl.touched).toBe(false);
+    service.displayValidationErrors(mockFormControl);
+    expect(mockFormControl.dirty).toBe(true);
+    expect(mockFormControl.touched).toBe(true);
+
+    for (const controlName in mockFormGroup.controls) {
+      const formControl = mockFormGroup.get(controlName);
+      expect(formControl.dirty).toBe(false);
+      expect(formControl.touched).toBe(false);
+    }
+    service.displayValidationErrors(mockFormGroup);
+    for (const controlName in mockFormGroup.controls) {
+      const formControl = mockFormGroup.get(controlName);
+      expect(formControl.dirty).toBe(true);
+      expect(formControl.touched).toBe(true);
+    }
+    const formArr = mockFormGroupWithFormArray.get('mockFormArray') as FormArray;
+    expect(formArr.dirty).toBe(false);
+    expect(formArr.touched).toBe(false);
+    service.displayValidationErrors(mockFormGroupWithFormArray);
+    expect(formArr.dirty).toBe(true);
+    expect(formArr.touched).toBe(true);
+    formArr.controls.forEach(element => {
+      expect(element.get('mockControl').dirty).toBe(true);
+      expect(element.get('mockControl').touched).toBe(true);
+    })
   });
 });
--- src/app/services/shared-methods.service.spec.ts
+++ src/app/services/shared-methods.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { SharedMethods } from './shared-methods.service';
@@ -7,6 +8,7 @@ import { SharedVar } from './shared-var.service';
 
 describe('SharedMethodsService', () => {
   let service: SharedMethods;
+  let routerSpy = {navigate: jasmine.createSpy('navigate')};
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,7 +17,8 @@ describe('SharedMethodsService', () => {
         RouterTestingModule
       ],
       providers: [
-        SharedVar
+        SharedVar,
+        { provide: Router, useValue: routerSpy }
       ]
     });
     service = TestBed.inject(SharedMethods);
@@ -23,5 +26,15 @@ describe('SharedMethodsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('navigate to create-trip route if create clicked button was pressed', () => {
+    service.showForm('create');
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['create-trip'], Object({ skipLocationChange: true }));
+  });
+
+  it('navigate to manage-trip route if manage clicked button was pressed', () => {
+    service.showForm('manage');
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['manage-trip'], Object({ skipLocationChange: true }));
   });
 });
